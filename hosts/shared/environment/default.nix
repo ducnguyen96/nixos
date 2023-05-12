ae2c96c68d7323c8198a5e7766eba4712a056d74
ducnguyen96 <levinguyen.dl@gmail.com>
2023-05-12 22:23:58 +0700

    changed: remove gnome-keyring from shellInit

--- hosts/shared/environment/default.nix
+++ hosts/shared/environment/default.nix
@@ -14,7 +14,6 @@
 
     loginShellInit = ''
       dbus-update-activation-environment --all
-      eval $(gnome-keyring-daemon --start --daemonize --components=pkcs11,secrets,ssh)
       eval $(ssh-agent)
     '';
   };
