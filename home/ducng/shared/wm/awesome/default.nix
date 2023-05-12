25fa02be7e55763c21712e5fc12001aaeb85f08c
ducnguyen96 <levinguyen.dl@gmail.com>
2023-05-13 00:35:36 +0700

    fix: awesome config

--- home/ducng/shared/wm/awesome/default.nix
+++ home/ducng/shared/wm/awesome/default.nix
@@ -1,13 +1,11 @@
 { pkgs, lib, config, ... }: {
   home = {
-    packages = [ pkgs.git ];
-    
     activation = {
       awesome = ''
         CONFIG="$HOME/.config/awesome"
 
         if [ ! -d "$CONFIG" ]; then
-          git clone https://github.com/ducnguyen96/rice-manual $CONFIG
+          ${pkgs.git}/bin/git clone https://github.com/ducnguyen96/rice-manual $CONFIG
         fi
       '';
     };
