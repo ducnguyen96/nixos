d206bbf84b20d7c2392e4467efb57a9786d7fa8e
ducnguyen96 <levinguyen.dl@gmail.com>
2023-05-13 00:10:14 +0700

    chore: test add awesome config file

--- home/ducng/shared/wm/awesome/default.nix
+++ home/ducng/shared/wm/awesome/default.nix
@@ -1,15 +1,7 @@
-{ config, pkgs, ... }:
+{ pkgs, lib, config, ... }:
 
 {
-  home = {
-    activation = {
-      awesome = ''
-        CONFIG="$HOME/.config/awesome"
-
-        if [ ! -d "$CONFIG" ]; then
-          git clone https://github.com/ducnguyen96/rice-manual $CONFIG
-        fi
-      '';
-    };
-  };
+  imports = [
+    ./rc.nix
+  ];
 }
