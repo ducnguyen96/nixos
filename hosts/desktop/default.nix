a88aa7b70a6d71355d818efd87f84a5e89d7290b
ducnguyen96 <levinguyen.dl@gmail.com>
2023-05-12 23:09:27 +0700

    chore: test awesome theme

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -53,6 +53,33 @@
 
       windowManager.awesome = {
         enable = true;
+        config = {
+          theme = {
+            # Colors
+            bg_normal = "#282c34";
+            fg_normal = "#abb2bf";
+            bg_focus = "#528bff";
+            fg_focus = "#ffffff";
+            bg_urgent = "#ff0000";
+            fg_urgent = "#ffffff";
+            # Font
+            font = "Roboto Mono 10";
+            # Taglist settings
+            taglist_bg_empty = "#282c34";
+            taglist_fg_empty = "#5b6268";
+            taglist_bg_occupied = "#282c34";
+            taglist_fg_occupied = "#c678dd";
+            taglist_bg_urgent = "#282c34";
+            taglist_fg_urgent = "#ff6c6b";
+            taglist_bg_focus = "#528bff";
+            taglist_fg_focus = "#ffffff";
+            # Layoutbox settings
+            layoutbox_bg_normal = "#282c34";
+            layoutbox_fg_normal = "#abb2bf";
+            layoutbox_bg_focus = "#528bff";
+            layoutbox_fg_focus = "#ffffff";
+          };
+        };
       };
 
       libinput = {
